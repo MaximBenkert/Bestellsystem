9f841e6b6b9d62e6d07a414c21fcd0098c1917b9
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-03-10 14:14:58 +0100

    Update OrderRepo

--- src/main/java/OrderRepo.java
+++ src/main/java/OrderRepo.java
@@ -1,2 +1,34 @@
+import java.util.List;
+
 public class OrderRepo {
+
+    import java.util.List;
+
+    public class OrderRepo {
+        private List<Order> orders;
+
+        public OrderRepo() {
+        }
+
+        public Order getOrderById (int OrderId, List<Order> orders){
+            for (Order : orders ){
+                if (OrderId == ) {
+                    return orders.get(OrderId);
+                }
+            }
+        }
+
+        public OrderRepo(List<Order> orders) {
+            this.orders = orders;
+        }
+
+        public List<Order> getOrders() {
+            return orders;
+        }
+
+        public void setOrders(List<Order> orders) {
+            this.orders = orders;
+        }
+    }
+
 }
